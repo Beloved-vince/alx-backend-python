83a7489dfcd643667a5217983e0e5b404302d860
Beloved-vince <vinceoludare@gmail.com>
2023-04-28 19:47:59 +0100

    variable annotation

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+    Variable annotation at it simplest form
+"""
+
+from math import floor
+
+
+def floor(n: float) -> int:
+    """ Returns the floor of the float """
+    return floor(n)
