1300ee077d60ecf42ea8298d62a41b1a5fe0ff2b
Beloved-vince <vinceoludare@gmail.com>
2023-04-28 19:49:16 +0100

    variable annotation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+    Variable annotation at it simplest form
+"""
+
+
+def to_str(n: float) -> str:
+    """ returns string representation of float type """
+    return str(n)
